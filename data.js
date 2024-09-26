55c15bf00d151e31080adc93c6afc7f86a59d6bc
awinash49 <161475857+awinash49@users.noreply.github.com>
2024-09-26 22:05:49 +0530

    Add files via upload
    added

---
+++ data.js
@@ -0,0 +1,65 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img2.jpg",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img1.jpg",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img3.jpg",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img5.jpg",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+  
+  export const skills = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "Python",
+    "Django",
+    "Firebase",
+  ];
+  
+  export const testimonials = [
+    {
+      quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "John Doe",
+      image: "./img6.jpg",
+    },
+    {
+      quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Jane Doe",
+      image: "./img7.jpg",
+    },
+    {
+      quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "Bob Smith",
+      image: "./img4.jpg",
+    },
+  ];
